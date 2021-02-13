c63e67ffad275341e3f88a94bb591f8fa17351dd
Giorgos Tsilivis <tsilivisgeo@gmail.com>
2021-02-13 22:10:22 +0200

    22.10

--- server.js
+++ server.js
@@ -72,8 +72,8 @@ db.close((err) => {
   console.log('Closed the database connection.');
 });
 
-// var PORTT = process.env.PORT || 8086;
-var PORTT = 8086
+var PORTT = process.env.PORT || 8086;
+// var PORTT = 8086
 const wss = new ws.Server({port:PORTT});
 
 
