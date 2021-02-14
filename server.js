4a034b4867b8b2090aa404f5ea9fde1bd96c6775
Giorgos Tsilivis <tsilivisgeo@gmail.com>
2021-02-14 04:35:24 +0200

    435

--- server.js
+++ server.js
@@ -66,7 +66,8 @@ let db = new sqlite3.Database('thebase.db', (err) => {
 });
 
 let rowarray = [];
-for (i = 1; i < 371585; i++) {
+// for (i = 1; i < 371585; i++) {
+for (i = 1; i < 10; i++) {
   let sql = `SELECT * FROM day WHERE rowid = ${i}`;
   db.all(sql, [], (err, rows) => {
     //console.log(rows)
